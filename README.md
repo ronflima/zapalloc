ea42c951549be33ac8ef32df68013e34fcb124fe
Ronaldo Faria Lima <ronaldo.faria.lima@gmail.com>
2016-12-10 19:45:11 -0200

    Added the test suite

--- README.md
+++ README.md
@@ -11,3 +11,15 @@ different size.
 
 This is not a memory debugger, but an attempt to optimize memory allocations by
 given the opportunity to alloc large amounts of memory and manage usage for you.
+
+## Fast or Crash
+
+This library was created with this in mind: be fast or crash. There is no memory
+debugging, no memory protection, no nothing. So, if your software invade memory,
+it will crash. So, be careful.
+
+# Simple, stupid
+
+This library was built over STDC malloc/calloc/realloc functions. It was
+designed to minimize system calls when returning memory to the user. Algorithms
+are really simple and are designed for speed. 
---
+++ src/testsuite/main.c
@@ -0,0 +1,49 @@
+/* -*-c-*-
+   The MIT License (MIT)
+
+   Copyright (c) 2016 - Ronaldo Faria Lima
+
+   Permission is hereby granted, free of charge, to any person obtaining a copy
+   of this software and associated documentation files (the "Software"), to deal
+   in the Software without restriction, including without limitation the rights
+   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+   copies of the Software, and to permit persons to whom the Software is
+   furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+   SOFTWARE.
+
+   Created: 2016-12-10 by Ronaldo Faria Lima
+
+   This file purpose: ZapAlloc test suite
+*/
+
+
+#include <stdio.h>
+#include "zapalloc.h"
+
+int
+main(void)
+{
+  zapalloc_context_t context;
+  if (zapalloc_init(&context, 1024, 10) != E_ZPC_OK)
+    {
+      fprintf(stderr, "Failed to initialize ZapAlloc Library\n");
+      return -0x1;
+    }
+  if (zapalloc_deinit(context) != E_ZPC_OK)
+    {
+      fprintf(stderr, "Failed do de-initializer ZapAlloc Library\n");
+      return -0x1;
+    }
+  printf("ZapAlloc Test Finished Successfully\n");
+  return 0x0;
+}
