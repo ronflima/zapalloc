6650ef079a78ca513dcb7c77dc9eb015544ca4e2
Ronaldo Faria Lima <ronaldo.faria.lima@gmail.com>
2016-12-15 17:55:18 -0200

    More travis CI updates

--- autogen.sh
+++ autogen.sh
@@ -1,17 +1,3 @@
 #!/bin/sh
 
-LIBTOOLIZE=glibtoolize
-if [ -x "$(command -v libtoolize)" ]
-then
-    LIBTOOLIZE=libtoolize
-fi
-
-$LIBTOOLIZE --version
-aclocal --version
-automake --version
-autoconf --version
-
-$LIBTOOLIZE
-aclocal
-automake --add-missing
-autoconf
+autoreconf -ivh
